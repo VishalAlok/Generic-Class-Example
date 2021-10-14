55a6fea850d2833ce09306ce22df90a105001d5c
vishal.Alok <you@example.com>
2021-10-14 06:23:22 +0530

    Add project files.

---
+++ Generic Class Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Generic_Class_Example
+{
+    class MyGenericClass<G,H>
+    {
+        public static bool EqualCheck(G a, H b)
+        {
+            bool c = a.Equals(b);
+            return c;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = MyGenericClass<int,string>.EqualCheck(10,"Hello");
+            Console.WriteLine(result);
+        }
+    }
+}
